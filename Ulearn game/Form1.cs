652593b527cebfae9899a65ffa5a44e3e00c5a0c
Ramil <stirk2281337@gmail.com>
2022-04-03 20:57:29 +0500

    added simple movement

--- Ulearn game/Form1.cs
+++ Ulearn game/Form1.cs
@@ -12,9 +12,76 @@ namespace Ulearn_game
 {
     public partial class Form1 : Form
     {
+        private PictureBox playerSprite;
+        private Player player;
         public Form1()
         {
             InitializeComponent();
+            Init();
+        }
+
+        public void Init()
+        {
+            player = new Player(100, Properties.Resources.icon);
+            playerSprite = new PictureBox();
+            playerSprite.Image = player.Sprite;
+            Controls.Add(playerSprite);
+
+            Timer tmr = new Timer();
+            tmr.Interval = 50;
+            KeyDown += new KeyEventHandler(Movement);
+            tmr.Start();
+        }
+        
+        public void Movement(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                playerSprite.Left += 1;
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                playerSprite.Left -= 1;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                playerSprite.Top -= 1;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                playerSprite.Top += 1;
+            }
+        }
+
+    }
+
+    class Entity
+    {
+        public int Health;
+        public int Damage;
+        public Image Sprite; 
+    } 
+
+    class Player : Entity
+    {
+        public Player(int health, Image sprite)
+        {
+            this.Health = health;
+            this.Sprite = sprite;
+        }
+
+    }
+
+    class Bandit : Entity
+    {
+        public Bandit()
+        {
+            Health = 20;
+            Damage = 10;
         }
     }
+
 }
